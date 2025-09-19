36f9b83aab604334302a5af9e4ce777c9f24e417
saniyashaikh0304 <77201633+saniyashaikh0304@users.noreply.github.com>
2025-09-19 23:18:11 +0530

    Time: 0 ms (100%), Space: 42 MB (48.75%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,24 +3,37 @@ import java.util.Arrays;
 
 class Solution {
     public int[] plusOne(int[] digits) {
-        String sum = "";
-		for (int i : digits) {
-			String value = String.valueOf(i);
-			sum += value;
-		}
-		
-		BigInteger num = new BigInteger(sum);
-	    num = num.add(BigInteger.ONE);
-	    String result = num.toString();
-		int length = result.length();
-		int[] output = new int[length];
-
-		for (int i = 0; i < result.length(); i++) {
-			char a = result.charAt(i);
-			int n = Integer.parseInt(String.valueOf(a));
-			output[i] = n;
+        for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i] += 1;
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
 		}
 
+		int[] output = new int[digits.length + 1];
+		output[0] = 1;
 		return output;
+
+        // String sum = "";
+		// for (int i : digits) {
+		// 	String value = String.valueOf(i);
+		// 	sum += value;
+		// }
+		
+		// BigInteger num = new BigInteger(sum);
+	    // num = num.add(BigInteger.ONE);
+	    // String result = num.toString();
+		// int length = result.length();
+		// int[] output = new int[length];
+
+		// for (int i = 0; i < result.length(); i++) {
+		// 	char a = result.charAt(i);
+		// 	int n = Integer.parseInt(String.valueOf(a));
+		// 	output[i] = n;
+		// }
+
+		// return output;
     }
 }
