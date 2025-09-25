3f0f5cacccef9d0ad6282adcf2ef147c75cea6a6
saniyashaikh0304 <77201633+saniyashaikh0304@users.noreply.github.com>
2025-09-25 23:20:41 +0530

    Time: 10 ms (2.7%), Space: 42.4 MB (5.26%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int output = 0;
+		String rem = "";
+		String str = String.valueOf(x);
+		if (str.startsWith("-")) {
+			rem = "-";
+			str = str.replace("-", "");
+		}
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char c = str.charAt(i);
+			rem = rem + c;
+		}
+		try {
+			output = Integer.valueOf(rem);			
+			return output;
+		} catch (Exception e) {
+			return 0;
+		}
+    }
+}
